7c27283eb71048d123377709b6f3640905474ff1
Kōhei Yamamoto <kymmt90@gmail.com>
2017-08-10 18:03:11 +0900

    Avoid N+1 when a customer is fetched for each order

--- app/graphql/loaders/record_loader.rb
+++ app/graphql/loaders/record_loader.rb
@@ -1,10 +1,10 @@
 class RecordLoader < GraphQL::Batch::Loader
-  def initialize(model)
-    @model = model
+  def initialize(model_name)
+    @model_name = model_name
   end
 
   def perform(ids)
-    @model.where(id: ids).each { |record| fulfill(record.id, record) }
+    @model_name.where(id: ids).each { |record| fulfill(record.id, record) }
     ids.each { |id| fulfill(id, nil) unless fulfilled?(id) }
   end
 end
--- app/graphql/types/order_type.rb
+++ app/graphql/types/order_type.rb
@@ -2,4 +2,9 @@ Types::OrderType = GraphQL::ObjectType.define do
   name 'Order'
 
   field :price, !types.Int
+  field :customer, !Types::CustomerType do
+    resolve ->(order, args, ctx) {
+      RecordLoader.for(Customer).load(order.customer_id)
+    }
+  end
 end
