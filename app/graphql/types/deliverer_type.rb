fce62594793f52ad5c3f40ee4646512f1555e95f
Kōhei Yamamoto <kymmt90@gmail.com>
2017-08-11 17:27:07 +0900

    Pass a key to HasManyAssociationLoader#load

--- app/graphql/types/deliverer_type.rb
+++ app/graphql/types/deliverer_type.rb
@@ -4,12 +4,12 @@ Types::DelivererType = GraphQL::ObjectType.define do
   field :name, !types.String
   connection :orders, Types::OrderType.connection_type do
     resolve ->(deliverer, args, ctx) {
-      Loaders::HasManyAssociationLoader.for(Order, :deliverer_id).load([deliverer.id])
+      Loaders::HasManyAssociationLoader.for(Order, :deliverer_id).load(deliverer.id)
     }
   end
   connection :customers, Types::CustomerType.connection_type do
     resolve ->(deliverer, args, ctx) {
-      Loaders::HasManyAssociationLoader.for(Order, :deliverer_id).load([deliverer.id]).then do |orders|
+      Loaders::HasManyAssociationLoader.for(Order, :deliverer_id).load(deliverer.id).then do |orders|
         Loaders::RecordLoader.for(Customer).load_many(orders.map(&:customer_id))
       end
     }
