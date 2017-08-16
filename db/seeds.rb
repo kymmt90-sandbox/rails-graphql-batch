7917958b25e64d96320681f1007a599c7ca9855c
Kōhei Yamamoto <kymmt90@gmail.com>
2017-08-17 00:26:15 +0900

    Improve seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
 puts 'Loading seeds...'
-user = User.new(email: 'foo@example.com')
-deliverer = Deliverer.create(name: 'Sagawa')
+user = User.create(email: 'foo@example.com')
+deliverers = %w(Sagawa Yamato).map { |name| Deliverer.create(name: name) }
 customer_attributes = %w(John Paul George Ringo).map { |name| { name: name } }
-customers = user.customers.build(customer_attributes)
-customers.each do |customer|
-  order_attributes = [100, 200, 300, 400, 500].map { |price| { price: price, deliverer: deliverer } }
-  customer.orders.build(order_attributes)
+customers = user.customers.create(customer_attributes)
+customers.each_with_index do |customer, index|
+  order_attributes = [100, 200, 300, 400, 500].map { |price|
+    [
+      {
+        price: price,
+        deliverer: deliverers[0]
+      },
+      {
+        price: price,
+        deliverer: deliverers[1]
+      }
+    ]
+  }.flatten
+  customer.orders.create(order_attributes)
 end
-user.save!
 puts 'Finished.'
